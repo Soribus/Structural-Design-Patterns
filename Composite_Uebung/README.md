130747fba593471183edd631328f09dace9d6d11
Soribus <47041295+Soribus@users.noreply.github.com>
2021-04-22 17:57:53 +0200

    finished composite task

--- Composite_Uebung/README.md
+++ Composite_Uebung/README.md
@@ -1,3 +1,22 @@
 # Composite_Uebung
 
+Stellen sie sich vor sie haben ein Unternehmen was Blätter verkauft (getrocknet und gefärbt).
+Für die Logistik sollen Versandpakete auf das Gramm genau berechnet werden.
+
+Jedes Paket kann einzelne Pakete beinhalten die entweder leer sind oder Blätter beinhalten.
+Bitte beachten sie meine klare Illustration:
+
+|---------------|
+|      |---|    |
+|      |*  |    |
+|      |---|    |
+|               |
+|---------------|
+
+Aufgabe:
+Erstellen sie einen Container der 2 eigene Container beinhaltet. 
+Diese beinhalten wiederum jeweils 2 Blätter(Leaf) mit unterschiedlichem Gewicht und einen weiteren Container mit 1 Blatt (Leaf).
+Von dem großen Container soll das Gesamtgewicht berechnet werden.
+
+Kurzform: Container(2 Container (2 Blatt + Container (1 Blatt)))
 
--- Composite_Uebung/src/Client.java
+++ Composite_Uebung/src/Client.java
@@ -1,7 +1,21 @@
 public class Client {
 
     public static void main(String[] args) {
-        //Create a container that has 3 smaller containers which each have 2 leaves with a different weight
+        // Zusammenfasssung der Aufgabe: Container(2 Container (2 Blatt + Container (1 Blatt)))
+
+        //Mein Vorschlag: erstellen sie zuerst die kleineren Container mit Blättern.
+        //Dann füge diese Container den mainContainer hinzu (sozusagen von klein zu groß arbeiten)
+        //Zum Abschluss die calculate Methode des großen Containers ausführen
+
+        Container mainContainer = new Container();
+
+        //insert code here
+
+        mainContainer.calculateWeight();
 
     }
+
+    //Frage: Warum kann ich Blätter und Container nicht einfach direkt dem Container als Child mitgeben?
+    //Wozu brauche ich das Interface in diesem Beispiel und wie würde es anders auch gehen?
+    //Bedenke dass ein Container sowohl andere Container als auch Blätter beinhalten kann.
 }
